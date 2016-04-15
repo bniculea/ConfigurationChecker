6f4aa2a3ad04745b13628be861fd7d3c45f58041
Bogdan Niculeasa <bogdan.niculeasa@hotmail.com>
2016-04-15 08:04:41 +0300

    Added project to git

---
+++ ConfigurationChecker/AssemblyInfoReader/AssemblyInfoReader.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Reflection;
+
+namespace AssemblyInfoReader
+{
+    public class AssemblyInfoReader
+    {
+        public void GetInfo(Assembly assembly)
+        {
+            assembly.
+        }
+    }
+}
